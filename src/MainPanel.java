6658dc1c53ada8d1abb589e39f4b7ee4019b1b1e
MohammadSadeghi99 <52172963+MohammadSadeghi99@users.noreply.github.com>
2019-07-04 11:16:08 +0430

    Update MainPanel.java

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -13,23 +13,36 @@ import java.util.Map;
 public class MainPanel extends JPanel {
     BufferedImage bufferedImage;
     BufferedImage bufferedImagebg;
-    BufferedImage blackPiece;
-    BufferedImage whitePiece;
+    BufferedImage blackPieceNotHighlighted;
+    BufferedImage whitePieceNotHighlighted;
+    BufferedImage blackPieceHighlight;
+    BufferedImage whitePieceHighlight;
+    BufferedImage blackPiece = whitePieceNotHighlighted;
+    BufferedImage whitePiece = blackPieceNotHighlighted;
+
     int X;
     int Y;
-    int z;
+    int movedFrom;
     int clickedPiece;
     boolean flag;
+    String turn;
 
     ArrayList<Piece> pieces = new ArrayList<>();
+    ArrayList<Piece>possiblePieces = new ArrayList<>();
     ArrayList<Place> piecesInPlace = new ArrayList<>();//according to the white place numbers
     Map <Integer , BoardCordinates> blackCordinator = new HashMap<Integer, BoardCordinates>();
     Map <Integer, BoardCordinates> whiteCordinator = new HashMap<Integer, BoardCordinates>();
 
     Dice dice;
+    Dice dice1;
+    Dice dice2;
+    Dice dice3;
     public MainPanel() {
 
+
+
         //15 black beads
+        pieces.add(null);
         pieces.add(new Piece(137, 465, true, "black"));
         pieces.add(new Piece(137, 420, true, "black"));
         pieces.add(new Piece(137, 375, true, "black"));
@@ -96,100 +109,6 @@ public class MainPanel extends JPanel {
         piecesInPlace.add(23, new Place(0, null));
         piecesInPlace.add(24, new Place(2, "white"));
 
-        setLayout(null);
-        try {
-            bufferedImagebg = ImageIO.read(new File("photos/1.jpg"));
-            bufferedImage = ImageIO.read(new File("photos/background.png"));
-            blackPiece = ImageIO.read(new File("photos/black_piece.png"));
-            whitePiece = ImageIO.read(new File("photos/white_piece.png"));
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-
-                if (dice.getMaxX() > e.getX() & dice.getMinX() < e.getX() & dice.getMaxY() > e.getY() & dice.getMinY() < e.getY()) {
-                    dice.randomGenerator();
-                    repaint();
-                }
-                // TODO: 6/28/2019 add showing possible pieces
-//                for (Piece piece:pieces){
-//                    if(!(piece.possible_positions_x==null) | !(piece.possible_positions_hit_x==null))
-//                        hidhlighit it!!
-//                }
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                clickedPiece = 0;
-                for (int i = 0; i < pieces.size(); i++) {
-
-                    // TODO: 6/28/2019 find possible positions of clicked piece
-
-
-                    flag = false;
-                    if (pieces.get(i).x < e.getX() && pieces.get(i).y < e.getY() && pieces.get(i).x + 46 > e.getX() && pieces.get(i).y + 46 > e.getY()) {
-
-                        clickedPiece = i;
-                        System.out.println(clickedPiece);
-                        flag = true;
-                        break;
-                    }
-                }
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-
-                if (flag && e.getY() < 490) {
-                    if (pieces.get(clickedPiece).color == "black") {
-                        for (Integer entry : blackCordinator.keySet()) {
-                            if (blackCordinator.get(entry).xcordinateBegin < e.getX() && blackCordinator.get(entry).xcordinateEnd > e.getX()) {
-                                pieces.get(clickedPiece).x = blackCordinator.get(entry).xcordinateMean;
-                                if(blackCordinator.get(clickedPiece).top)
-                                    pieces.get(clickedPiece).y = piecesInPlace.get(entry).number*45 + 28;
-                                    // TODO: 6/28/2019 update each cordinate number of pieces
-                                else
-                                    pieces.get(clickedPiece).y = 465- piecesInPlace.get(entry).number*45;
-
-                                repaint();
-                                break;
-                            }
-                        }
-                    }
-                    if (pieces.get(clickedPiece).color == "white") {
-                        for (Integer entry : whiteCordinator.keySet()) {
-                            if (blackCordinator.get(entry).xcordinateBegin < e.getX() && blackCordinator.get(entry).xcordinateEnd > e.getX()) {
-                                pieces.get(clickedPiece).x = whiteCordinator.get(entry).xcordinateMean;
-                                if(blackCordinator.get(clickedPiece).top)
-                                    pieces.get(clickedPiece).y = piecesInPlace.get(entry).number*45 + 28;
-                                else
-                                    pieces.get(clickedPiece).y = 465- piecesInPlace.get(entry).number*45;
-                                repaint();
-                                break;
-                            }
-
-                        }
-
-                    }
-                }
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
-        });
-        dice = new Dice(50, 700);
-
-
         //cordinates for black
         blackCordinator.put(1, new BoardCordinates(749, 795, true));
         blackCordinator.put(2, new BoardCordinates(695, 742, true));
@@ -241,6 +160,160 @@ public class MainPanel extends JPanel {
         whiteCordinator.put(15, new BoardCordinates(220, 266, true));
         whiteCordinator.put(14, new BoardCordinates(167, 213, true));
         whiteCordinator.put(13, new BoardCordinates(114, 160, true));
+
+        setLayout(null);
+        try {
+            bufferedImagebg = ImageIO.read(new File("photos/1.jpg"));
+            bufferedImage = ImageIO.read(new File("photos/background.png"));
+            blackPieceNotHighlighted = ImageIO.read(new File("photos/black_piece.png"));
+            whitePieceNotHighlighted = ImageIO.read(new File("photos/white_piece.png"));
+            blackPieceHighlight = ImageIO.read(new File("photos/black_piece_highlighted.png"));
+            whitePieceHighlight = ImageIO.read(new File("photos/white_piece_highlighted.png"));
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                if ((dice.getMaxX() > e.getX() & dice.getMinX() < e.getX() & dice.getMaxY() > e.getY() & dice.getMinY() < e.getY())|(dice1.getMaxX() > e.getX() & dice1.getMinX() < e.getX() & dice1.getMaxY() > e.getY() & dice1.getMinY() < e.getY())) {
+                    dice.randomGenerator();
+                    dice1.randomGenerator();
+                    repaint();
+                }
+                                                                        //!!!after each move, possible positions should become null
+                turn = "white";
+                if (turn == "white") {
+                    for (int i = 1; i < 25; i++) {
+
+                        if ((i-dice.getRand()>0 && !(piecesInPlace.get(i - dice.getRand()).color == "black")) || (i-dice1.getRand()>0 && !(piecesInPlace.get(i - dice1.getRand()).color == "black"))) { //move without hit
+                            for (int j = 16; j < 31; j++) {
+                                if (pieces.get(j).x == whiteCordinator.get(i).xcordinateMean) {
+                                    possiblePieces.add(pieces.get(j));
+                                    pieces.get(j).possible_positions_x.add(whiteCordinator.get(i).xcordinateMean);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // TODO: 6/28/2019 add showing possible pieces
+//                for (int i = 1; i < 31; i++) {
+//                    if (!(pieces.get(i).possible_positions_x==null) | !(piece.possible_positions_hit_x==null))
+//                        highlighit it!!
+//                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                clickedPiece = 0;
+                for (int i = 1; i < 31; i++) {
+
+                    //update pieces in place for moved piece
+                    // TODO: 6/28/2019 find possible positions of clicked piece
+
+                    flag = false;
+                    if (pieces.get(i).x < e.getX() && pieces.get(i).y < e.getY() && pieces.get(i).x + 46 > e.getX() && pieces.get(i).y + 46 > e.getY()) {
+
+                        clickedPiece = i;
+
+                        for (int j = 1; j <25 ; j++) {
+                            if (whiteCordinator.get(j).xcordinateBegin < e.getX() && whiteCordinator.get(j).xcordinateEnd > e.getX()) {
+                                movedFrom = j;
+                                break;
+                            }
+                        }
+
+                        flag = true;                //means that a piece has been clicked
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (flag && e.getY() < 490) {                           //a piece has been clicked and now it is releasing in the board
+                    if (pieces.get(clickedPiece).color == "black") {
+                        for (Integer entry : blackCordinator.keySet()) {
+                            if (blackCordinator.get(entry).xcordinateBegin < e.getX() && blackCordinator.get(entry).xcordinateEnd > e.getX()) {
+                                pieces.get(clickedPiece).oldx = pieces.get(clickedPiece).x;
+                                pieces.get(clickedPiece).x = blackCordinator.get(entry).xcordinateMean;
+
+                                if (piecesInPlace.get(movedFrom).number > 0)
+                                    piecesInPlace.get(movedFrom).number--;
+                                if (piecesInPlace.get(movedFrom).number == 0)
+                                    piecesInPlace.get(movedFrom).color = null;
+
+                                if(e.getY()<245) {
+                                    pieces.get(clickedPiece).oldy = pieces.get(clickedPiece).y;
+                                    pieces.get(clickedPiece).y = piecesInPlace.get(25-entry).number * 45 + 28;
+                                }
+                                else {
+                                    pieces.get(clickedPiece).oldy = pieces.get(clickedPiece).y;
+                                    pieces.get(clickedPiece).y = 465 - piecesInPlace.get(25-entry).number * 45;
+                                }
+
+                                piecesInPlace.get(entry).number++;
+                                piecesInPlace.get(entry).color = "black";
+                                repaint();
+                                break;
+                            }
+
+                        }
+                    }
+                    if (pieces.get(clickedPiece).color == "white") {
+                        for (Integer entry : whiteCordinator.keySet()) {
+                            if (whiteCordinator.get(entry).xcordinateBegin < e.getX() && whiteCordinator.get(entry).xcordinateEnd > e.getX()) {
+                                pieces.get(clickedPiece).oldx = pieces.get(clickedPiece).x;
+                                pieces.get(clickedPiece).x = whiteCordinator.get(entry).xcordinateMean;
+
+                                if (piecesInPlace.get(movedFrom).number > 0)
+                                    piecesInPlace.get(movedFrom).number--;
+                                if (piecesInPlace.get(movedFrom).number == 0)
+                                    piecesInPlace.get(movedFrom).color = null;
+
+                                if(e.getY()<245) {
+                                    pieces.get(clickedPiece).oldy = pieces.get(clickedPiece).y;
+                                    pieces.get(clickedPiece).y = piecesInPlace.get(entry).number * 45 + 28;
+                                }
+                                else {
+                                    pieces.get(clickedPiece).oldy = pieces.get(clickedPiece).y;
+                                    pieces.get(clickedPiece).y = 465 - piecesInPlace.get(entry).number * 45;
+                                }
+
+                                piecesInPlace.get(entry).number++;
+                                piecesInPlace.get(entry).color = "white";
+                                repaint();
+                                break;
+                            }
+
+                        }
+
+                    }
+                }
+                for (int i = 1; i < 25; i++) {
+                    System.out.println(piecesInPlace.get(i).number + ";;;");
+
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        dice =  new Dice(50, 700);
+        dice1 = new Dice(100,700);
+        dice2 = new Dice(150,700);
+        dice3 = new Dice(200,700);
+
     }
     @Override
     protected void paintComponent(Graphics g) {
@@ -250,15 +323,28 @@ public class MainPanel extends JPanel {
         g.drawImage(bufferedImage, 0, 0, null);
         g.setColor(Color.BLACK);
         dice.paint((Graphics2D) g);
+        dice1.paint((Graphics2D) g);
+        if(dice.getRand()==dice1.getRand()){
+            dice2.setRand(dice.getRand());
+            dice3.setRand(dice1.getRand());
+            dice2.paint((Graphics2D) g);
+            dice3.paint((Graphics2D) g);
 
-        for (Piece piece : pieces) {
-            if(piece.color == "black"){
-                g.drawImage(blackPiece, piece.x, piece.y, null);
+
+        }
+
+
+
+        for (int i = 1; i < 31; i++) {
+
+            if(pieces.get(i).color == "black"){
+                g.drawImage(blackPieceNotHighlighted, pieces.get(i).x, pieces.get(i).y, null);
             }
             else{
-                g.drawImage(whitePiece, piece.x, piece.y, null);
+                g.drawImage(whitePieceNotHighlighted, pieces.get(i).x, pieces.get(i).y, null);
             }
         }
 
     }
+
 }
